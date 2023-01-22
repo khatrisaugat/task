c0c04e3e40c99a1474db5658b1ac4beb767062af
khatrisaugat <khatri.saugat55@gmail.com>
2023-01-22 22:29:29 +0545

    db exception handling

--- backend/src/api/helpers/Database/index.js
+++ backend/src/api/helpers/Database/index.js
@@ -16,9 +16,13 @@ class Database {
     const tableName = this.tableName;
     const columns = this.columns;
     const sql = table.createTable(tableName, columns);
-    const connection = await db.connectdb();
-    const [rows, fields] = await connection.query(sql);
-    connection.end();
+    try {
+      const connection = await db.connectdb();
+      const [rows, fields] = await connection.query(sql);
+      connection.end();
+    } catch (error) {
+      console.log("error: ", error);
+    }
     // await connection.query(sql);
     // console.log(rows);
   };
@@ -42,49 +46,64 @@ class Database {
   create = async (data) => {
     const tableName = this.tableName;
     const sql = crud.create(tableName, data);
-    const connection = await db.connectdb();
-    const [rows, fields] = await connection.query(sql);
-    connection.end();
-    // console.log("rows: ", rows);
-    return rows.insertId;
+    try {
+      const connection = await db.connectdb();
+      const [rows, fields] = await connection.query(sql);
+      connection.end();
+      // console.log("rows: ", rows);
+      return rows.insertId;
+    } catch (error) {
+      console.log("error: ", error);
+    }
   };
 
   //get data from table
   find = async (condition) => {
     const tableName = this.tableName;
     const sql = crud.read(tableName, condition);
-
-    const connection = await db.connectdb();
-    const [rows] = await connection.query(sql);
-    connection.end();
-    // console.log("rowsData: ", rows);
-    return rows;
+    try {
+      const connection = await db.connectdb();
+      const [rows] = await connection.query(sql);
+      connection.end();
+      // console.log("rowsData: ", rows);
+      return rows;
+    } catch (error) {
+      console.log("error: ", error);
+    }
   };
 
   //update data in table
   update = async (data, where) => {
     const tableName = this.tableName;
     const sql = crud.update(tableName, data, where);
-    const connection = await db.connectdb();
-    const [rows] = await connection.query(sql);
-    // console.log("rowsData: ", rows);
-    if (rows.affectedRows == 0) return { error: "No data found to update" };
-    const result = await this.find(where);
-    connection.end();
-    console.log("result: ", result);
-    return result;
+    try {
+      const connection = await db.connectdb();
+      const [rows] = await connection.query(sql);
+      // console.log("rowsData: ", rows);
+      if (rows.affectedRows == 0) return { error: "No data found to update" };
+      const result = await this.find(where);
+      connection.end();
+      console.log("result: ", result);
+      return result;
+    } catch (error) {
+      console.log("error: ", error);
+    }
   };
 
   //delete data from table
   delete = async (where) => {
     const tableName = this.tableName;
     const sql = crud.delete(tableName, where);
-    const connection = await db.connectdb();
-    const [rows] = await connection.query(sql);
-    connection.end();
-    // console.log("rowsData: ", rows);
-    if (rows.affectedRows == 0) return { error: "No data found to delete" };
-    return { message: "Data deleted successfully" };
+    try {
+      const connection = await db.connectdb();
+      const [rows] = await connection.query(sql);
+      connection.end();
+      // console.log("rowsData: ", rows);
+      if (rows.affectedRows == 0) return { error: "No data found to delete" };
+      return { message: "Data deleted successfully" };
+    } catch (error) {
+      console.log("error: ", error);
+    }
   };
 }
 
