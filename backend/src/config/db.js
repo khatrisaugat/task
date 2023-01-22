1376a41cf0b70b195918cdcf5b7c1e5043b9bea2
khatrisaugat <khatri.saugat55@gmail.com>
2023-01-22 22:34:14 +0545

    little changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,10 +2,10 @@ const connectdb = async () => {
   var mysql = require("mysql2/promise");
   //local mysql db connection
   const connection = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_DATABASE || "cloco",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   });
   // console.log("connection", connection);
   return connection;
